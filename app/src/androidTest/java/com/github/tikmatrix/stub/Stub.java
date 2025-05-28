c844191609b9083c15eb563b1be4c837cdd72fd3
niostack <niostack@gmail.com>
2025-05-28 14:28:46 +0800

    003

--- app/src/androidTest/java/com/github/tikmatrix/stub/Stub.java
+++ app/src/androidTest/java/com/github/tikmatrix/stub/Stub.java
@@ -104,10 +104,12 @@ public class Stub {
             Log.i("runWatchers");
         }
     }
+
     void runWatchers() {
         AutomatorService automatorService = server.getAutomatorService();
         automatorService.runWatchers();
     }
+
     boolean isWatcherRegistered(String watcherName) {
         AutomatorService automatorService = server.getAutomatorService();
         String[] registeredWatchers = automatorService.getWatchers();
@@ -118,57 +120,64 @@ public class Stub {
         }
         return false;
     }
+
     void registerWatchers() {
         AutomatorService automatorService = server.getAutomatorService();
         String[] registeredWatchers = automatorService.getWatchers();
 
-        //check if MatchBack is registered
+        // check if MatchBack is registered
         if (!isWatcherRegistered("MatchBack")) {
-            Selector conditions=new Selector().textMatches("Read status|Post view history turned on|Profile view history turned on|View your friends’ posts|Chat with more people|Feed accessibility tool is on|See who is online now|Posts are from followers that you follow back|Follow your friends|Let's do a quick security checkup|Create your TikTok avatar");
-            String[] keys={"back"};
-            automatorService.registerPressKeyskWatcher("MatchBack", new Selector[] {conditions}, keys);
+            Selector conditions = new Selector().textMatches(
+                    "Read status|Post view history turned on|Profile view history turned on|View your friends’ posts|Chat with more people|Feed accessibility tool is on|See who is online now|Posts are from followers that you follow back|Follow your friends|Let's do a quick security checkup|Create your TikTok avatar");
+            String[] keys = { "back" };
+            automatorService.registerPressKeyskWatcher("MatchBack", new Selector[] { conditions }, keys);
         }
-        //check if Match_v5 is registered
+        // check if Match_v5 is registered
         if (!isWatcherRegistered("Match_v5")) {
-            Selector conditions=new Selector().textMatches("Your friends on TikTok|Dismiss|Start watching|Don’t allow|Continue anyway|Post anyway|Not now|Agree and continue|Accept|Got it|Remind me later|Choose how ads are shown|Choose your interests|Viewer history turned on|TikTok isn't responding");
-            Selector target=new Selector().textMatches("Done|Dismiss|Start watching|Don’t allow|Skip|Continue anyway|Post anyway|Not now|Agree and continue|Accept|Got it|Remind me later|Select|Save|Close app");
-            automatorService.registerClickUiObjectWatcher("Match_v5", new Selector[] {conditions}, target);
+            Selector conditions = new Selector().textMatches(
+                    "Your friends on TikTok|Dismiss|Start watching|Don’t allow|Continue anyway|Post anyway|Not now|Agree and continue|Accept|Got it|Remind me later|Choose how ads are shown|Choose your interests|Viewer history turned on|TikTok isn't responding");
+            Selector target = new Selector().textMatches(
+                    "Done|Dismiss|Start watching|Don’t allow|Skip|Continue anyway|Post anyway|Not now|Agree and continue|Accept|Got it|Remind me later|Select|Save|Close app");
+            automatorService.registerClickUiObjectWatcher("Match_v5", new Selector[] { conditions }, target);
         }
-        //check if MatchCancel is registered
-        if (!isWatcherRegistered("MatchCancel")) {
-            Selector conditions=new Selector().textMatches("Your support means a lot to creators|Keep|How do you feel about the video you just watched?|Authorize|How do you feel about the video you just watched?");
-            Selector target=new Selector().text("Cancel");
-            automatorService.registerClickUiObjectWatcher("MatchCancel", new Selector[] {conditions}, target);
+        // check if MatchCancel is registered
+        if (!isWatcherRegistered("MatchCancel_v1")) {
+            Selector conditions = new Selector().textMatches(
+                    "Keep editing your unposted video?|Your support means a lot to creators|Keep|How do you feel about the video you just watched?|Authorize|How do you feel about the video you just watched?");
+            Selector target = new Selector().text("Cancel");
+            automatorService.registerClickUiObjectWatcher("MatchCancel", new Selector[] { conditions }, target);
         }
-        //check if MatchOk is registered
+        // check if MatchOk is registered
         if (!isWatcherRegistered("MatchOk")) {
-            Selector conditions=new Selector().textMatches("Review your date of birth|Introducing 10 minutes video|View your friends’ posts");
-            Selector target=new Selector().text("OK");
-            automatorService.registerClickUiObjectWatcher("MatchOk", new Selector[] {conditions}, target);
+            Selector conditions = new Selector()
+                    .textMatches("Review your date of birth|Introducing 10 minutes video|View your friends’ posts");
+            Selector target = new Selector().text("OK");
+            automatorService.registerClickUiObjectWatcher("MatchOk", new Selector[] { conditions }, target);
         }
-        //check if MatchConfirm is registered
-        if (!isWatcherRegistered("MatchConfirm")) {        
-            Selector conditions=new Selector().textMatches("Change|What languages do you understand");
-            Selector target=new Selector().text("Confirm");
-            automatorService.registerClickUiObjectWatcher("MatchConfirm", new Selector[] {conditions}, target);
+        // check if MatchConfirm is registered
+        if (!isWatcherRegistered("MatchConfirm")) {
+            Selector conditions = new Selector().textMatches("Change|What languages do you understand");
+            Selector target = new Selector().text("Confirm");
+            automatorService.registerClickUiObjectWatcher("MatchConfirm", new Selector[] { conditions }, target);
         }
-        //check if Discard is registered
+        // check if Discard is registered
         if (!isWatcherRegistered("Discard")) {
-            Selector conditions=new Selector().text("Edit");
-            Selector target=new Selector().text("Discard");
-            automatorService.registerClickUiObjectWatcher("Discard", new Selector[] {conditions}, target);
+            Selector conditions = new Selector().text("Edit");
+            Selector target = new Selector().text("Discard");
+            automatorService.registerClickUiObjectWatcher("Discard", new Selector[] { conditions }, target);
         }
-        //check if permission_allow_button is registered
+        // check if permission_allow_button is registered
         if (!isWatcherRegistered("permission_allow_button")) {
-            Selector conditions=new Selector().text("Allow");
-            Selector target=new Selector().text("Allow");
-            automatorService.registerClickUiObjectWatcher("permission_allow_button", new Selector[] {conditions}, target);
+            Selector conditions = new Selector().text("Allow");
+            Selector target = new Selector().text("Allow");
+            automatorService.registerClickUiObjectWatcher("permission_allow_button", new Selector[] { conditions },
+                    target);
         }
-        //instagram Terms and Privacy Policy
+        // instagram Terms and Privacy Policy
         if (!isWatcherRegistered("Terms")) {
-            Selector conditions=new Selector().text("Terms and Privacy Policy");
-            Selector target=new Selector().text("Continue");
-            automatorService.registerClickUiObjectWatcher("Terms", new Selector[] {conditions}, target);
+            Selector conditions = new Selector().text("Terms and Privacy Policy");
+            Selector target = new Selector().text("Continue");
+            automatorService.registerClickUiObjectWatcher("Terms", new Selector[] { conditions }, target);
         }
     }
 }
